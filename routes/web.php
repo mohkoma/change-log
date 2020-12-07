d8596ee493c1dfa303db2f319aee6aaaef3efda1
Mohammed Sellami <mohammed@ninjami.com>
2020-12-07 10:24:26 +0200

    Fix controller name issue

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,19 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Mohkoma\ChangeLog\Controllers', 'prefix' => 'dev', 'middleware' => config('changelog.middleware.read')], function() {
 
     // Show as view
-    Route::get('changelog', "ChangelogController@show")->name('changelog.view');
+    Route::get('changelog', "ChangeLogController@show")->name('changelog.view');
 
     // Show as json
-    Route::get('changelog/json', "ChangelogController@index")->name('changelog.index');
+    Route::get('changelog/json', "ChangeLogController@index")->name('changelog.index');
 
     // Write
     Route::group(['middleware' => config('changelog.middleware.write')], function() {
 
         // show the form
-        Route::get('changelog/create', "ChangelogController@create")->name('changelog.create');
+        Route::get('changelog/create', "ChangeLogController@create")->name('changelog.create');
 
         // Store new release
-        Route::post('changelog', "ChangelogController@store")->name('changelog.store');
+        Route::post('changelog', "ChangeLogController@store")->name('changelog.store');
 
     });
 });
